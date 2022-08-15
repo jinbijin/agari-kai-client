8b60a3306ea5f3c15ebdbb1406ab5aeecc27985a
jinbijin <35583057+jinbijin@users.noreply.github.com>
2022-08-15 22:12:19 +0200

    Initialize Angular app

---
+++ src/app/agari.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AgariRoutingModule } from './agari.routing-module';
+import { AgariComponent } from './agari.component';
+
+@NgModule({
+  declarations: [
+    AgariComponent
+  ],
+  imports: [
+    BrowserModule,
+    AgariRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AgariComponent]
+})
+export class AgariModule { }
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AgariModule } from './app/agari.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AgariModule)
+  .catch(err => console.error(err));
