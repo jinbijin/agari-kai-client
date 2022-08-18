ee53cb484940d90ff88d66ca450704f05ea8c94d
jinbijin <35583057+jinbijin@users.noreply.github.com>
2022-08-18 23:06:21 +0200

    Apply prettier

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   projectId: '1sq9ou',
   e2e: {
-    'baseUrl': 'http://localhost:4200',
+    baseUrl: 'http://localhost:4200',
   },
-})
+});
