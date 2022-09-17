9c3e41d21a6e1e0b8f5967f47ac990a7cb48d925
jinbijin <35583057+jinbijin@users.noreply.github.com>
2022-09-17 20:40:35 +0200

    Move routes definition into token definition

--- src/app/routing/agari.routing-module.ts
+++ src/app/routing/agari.routing-module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, ROUTES, TitleStrategy } from '@angular/router';
 import { UnderConstructionComponent } from '../modules/under-construction/under-construction.component';
-import { agariRoutes } from './agari.routes';
 import { AgariTitleRenderer } from './agari.title-renderer';
 import { AgariTitleStrategy } from './agari.title-strategy';
 import { PROVIDE_AGARI_NAVIGATION_ITEMS } from './tokens/agari-navigation-items.token';
-import { AGARI_ROUTES } from './tokens/agari-routes.token';
+import { AGARI_ROUTES, PROVIDE_AGARI_ROUTES } from './tokens/agari-routes.token';
 
 @NgModule({
   imports: [RouterModule.forRoot([]), UnderConstructionComponent],
@@ -13,8 +12,8 @@ import { AGARI_ROUTES } from './tokens/agari-routes.token';
   providers: [
     AgariTitleRenderer,
     { provide: TitleStrategy, useClass: AgariTitleStrategy },
-    { provide: AGARI_ROUTES, useValue: agariRoutes },
     { provide: ROUTES, useExisting: AGARI_ROUTES, multi: true },
+    PROVIDE_AGARI_ROUTES,
     PROVIDE_AGARI_NAVIGATION_ITEMS,
   ],
 })
--- src/app/routing/tokens/agari-routes.token.ts
+++ src/app/routing/tokens/agari-routes.token.ts
@@ -1,4 +1,17 @@
-import { InjectionToken } from '@angular/core';
+import { InjectionToken, Provider } from '@angular/core';
+import { UnderConstructionComponent } from 'src/app/modules/under-construction/under-construction.component';
 import { AgariRoute } from './agari-route.type';
 
 export const AGARI_ROUTES: InjectionToken<AgariRoute[]> = new InjectionToken<AgariRoute[]>('agari-routes');
+
+const agariRoutes: AgariRoute[] = [
+  { path: '', pathMatch: 'full', component: UnderConstructionComponent, data: { menu: { icon: 'home', label: 'Home' } } },
+  {
+    path: 'tournaments',
+    component: UnderConstructionComponent,
+    title: 'Tournaments',
+    data: { menu: { icon: 'table_view', label: 'Tournaments' } },
+  },
+];
+
+export const PROVIDE_AGARI_ROUTES: Provider = { provide: AGARI_ROUTES, useValue: agariRoutes };
