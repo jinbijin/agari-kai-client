0403d1a842c2c54e8a50b852b1ed038a5d913eb7
jinbijin <35583057+jinbijin@users.noreply.github.com>
2022-09-17 20:35:12 +0200

    Extract factory

--- src/app/routing/agari-navigation-items.token.ts
+++ src/app/routing/agari-navigation-items.token.ts
@@ -1,7 +1,26 @@
-import { InjectionToken } from '@angular/core';
-import { Observable } from 'rxjs';
+import { InjectionToken, Provider } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { AgariNavigationItem } from './agari-navigation-item.type';
+import { AgariRoute } from './agari-route.type';
+import { AGARI_ROUTES } from './agari-routes.token';
 
 export const AGARI_NAVIGATION_ITEMS: InjectionToken<Observable<AgariNavigationItem[]>> = new InjectionToken<
   Observable<AgariNavigationItem[]>
 >('agari-navigation-items');
+
+function createNavigationItems(agariRoutes: AgariRoute[]): Observable<AgariNavigationItem[]> {
+  return of(
+    agariRoutes.map((agariRoute) => ({
+      icon: agariRoute.data.menu.icon,
+      label: agariRoute.data.menu.label,
+      routerLink: agariRoute.path ? ['/', agariRoute.path] : ['/'],
+      routerLinkActiveOptions: { exact: agariRoute.pathMatch === 'full' },
+    }))
+  );
+}
+
+export const PROVIDE_AGARI_NAVIGATION_ITEMS: Provider = {
+  provide: AGARI_NAVIGATION_ITEMS,
+  useFactory: createNavigationItems,
+  deps: [AGARI_ROUTES],
+};
--- src/app/routing/agari.routing-module.ts
+++ src/app/routing/agari.routing-module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, ROUTES, TitleStrategy } from '@angular/router';
-import { of } from 'rxjs';
 import { UnderConstructionComponent } from '../modules/under-construction/under-construction.component';
-import { AgariNavigationItem } from './agari-navigation-item.type';
-import { AGARI_NAVIGATION_ITEMS } from './agari-navigation-items.token';
-import { AgariRoute } from './agari-route.type';
+import { PROVIDE_AGARI_NAVIGATION_ITEMS } from './agari-navigation-items.token';
 import { AGARI_ROUTES } from './agari-routes.token';
 import { agariRoutes } from './agari.routes';
 import { AgariTitleRenderer } from './agari.title-renderer';
@@ -18,19 +15,7 @@ import { AgariTitleStrategy } from './agari.title-strategy';
     { provide: TitleStrategy, useClass: AgariTitleStrategy },
     { provide: AGARI_ROUTES, useValue: agariRoutes },
     { provide: ROUTES, useExisting: AGARI_ROUTES, multi: true },
-    {
-      provide: AGARI_NAVIGATION_ITEMS,
-      useFactory: (agariRoutes: AgariRoute[]) =>
-        of(
-          agariRoutes.map<AgariNavigationItem>((agariRoute) => ({
-            icon: agariRoute.data.menu.icon,
-            label: agariRoute.data.menu.label,
-            routerLink: agariRoute.path ? ['/', agariRoute.path] : ['/'],
-            routerLinkActiveOptions: { exact: agariRoute.pathMatch === 'full' },
-          }))
-        ),
-      deps: [AGARI_ROUTES],
-    },
+    PROVIDE_AGARI_NAVIGATION_ITEMS,
   ],
 })
 export class AgariRoutingModule {}
