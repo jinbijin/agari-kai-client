8a3e0a2eba14ed549c425c3b9b6eb7f50213cc5e
jinbijin <35583057+jinbijin@users.noreply.github.com>
2022-09-18 17:03:46 +0200

    Fix url check

--- cypress/e2e/tournaments.cy.ts
+++ cypress/e2e/tournaments.cy.ts
@@ -2,6 +2,9 @@ describe('Tournaments landing', () => {
   it('navigates via sidebar to landing', () => {
     cy.visit('/');
     cy.get('[data-test-id="navigation-link-tournaments"]').click();
-    cy.url().should('equal', `${Cypress.config().baseUrl}tournaments`);
+    cy.url().should((url) => {
+      const expectedUrl = `${Cypress.config().baseUrl}/tournaments`;
+      expect(url).to.equal(expectedUrl.replace(/(?<!:)\/\//g, '/'));
+    });
   });
 });
