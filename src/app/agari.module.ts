008c5ba28c256f4c7143c795c99fe410620986f8
jinbijin <35583057+jinbijin@users.noreply.github.com>
2022-09-26 23:11:35 +0200

    Remove @material/web

--- src/app/agari.module.ts
+++ src/app/agari.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { AgariComponent } from './agari.component';
 import { AgariSidebarComponent } from './modules/sidebar/sidebar.component';
 import { AgariRoutingModule } from './routing/agari.routing-module';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AgariComponent],
@@ -16,8 +16,6 @@ import { environment } from '../environments/environment';
     AgariSidebarComponent,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
   ],
