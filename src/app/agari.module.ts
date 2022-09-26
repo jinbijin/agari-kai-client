cb05b8679b04b9c220bfb051f352ea2995905677
jinbijin <35583057+jinbijin@users.noreply.github.com>
2022-09-26 21:18:18 +0200

    Add PWA functionality and format

--- src/app/agari.module.ts
+++ src/app/agari.module.ts
@@ -4,10 +4,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AgariComponent } from './agari.component';
 import { AgariSidebarComponent } from './modules/sidebar/sidebar.component';
 import { AgariRoutingModule } from './routing/agari.routing-module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AgariComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AgariRoutingModule, AgariSidebarComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AgariRoutingModule,
+    AgariSidebarComponent,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   bootstrap: [AgariComponent],
 })
 export class AgariModule {}
