32e8470d8abfdc23863299723938d122f182436b
jinbijin <35583057+jinbijin@users.noreply.github.com>
2022-10-03 23:09:11 +0200

    Implement navigation part of new button

--- src/app/modules/tournament/overview-actions/overview-actions.component.ts
+++ src/app/modules/tournament/overview-actions/overview-actions.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DateTime } from 'luxon';
 import { toUtcDateTime } from 'src/app/common/utc-date-time';
 import { uuid } from 'src/app/common/uuid';
@@ -15,10 +16,12 @@ import { AgariDb } from 'src/app/core/data/schema/agari.db';
 })
 export class TournamentOverviewActionsComponent {
   readonly #db = inject(AgariDb);
+  readonly #router = inject(Router);
+  readonly #activatedRoute = inject(ActivatedRoute);
 
   async createTournament(): Promise<void> {
     const tournamentId = uuid();
     await this.#db.tournaments.add({ _id: tournamentId, _updatedAt: toUtcDateTime(DateTime.now()) });
-    // TODO navigate
+    await this.#router.navigate([tournamentId, 'edit'], { relativeTo: this.#activatedRoute });
   }
 }
