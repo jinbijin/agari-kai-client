d9f43b035257a42f50673f0e9c405d5895d9852b
jinbijin <35583057+jinbijin@users.noreply.github.com>
2022-10-04 20:35:18 +0200

    Add new types to schema

--- src/app/core/data/schema/agari.db.ts
+++ src/app/core/data/schema/agari.db.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Dexie, Table } from 'dexie';
-import { Entity } from './types/entity.type';
+import { Uuid } from 'src/app/common/uuid';
+import { Datum } from './types/datum.type';
+import { Entity, EntityRecord } from './types/entity.type';
+import { Revision } from './types/revision.type';
 import { Tournament } from './types/tournament.type';
 
 export interface AgariDb {
   tournaments: Table<Entity<Tournament>, number>;
+  entities: Table<EntityRecord, Uuid>;
+  data: Table<Datum, Uuid>;
+  revisions: Table<Revision, number>;
 }
 
 @Injectable()
--- src/app/core/data/schema/types/entity.type.ts
+++ src/app/core/data/schema/types/entity.type.ts
@@ -1,6 +1,14 @@
 import { UtcDateTime } from 'src/app/common/utc-date-time';
 import { Uuid } from 'src/app/common/uuid';
 
+type EntityType = 'tournament';
+
+// TODO rename to Entity when other type is moved
+export interface EntityRecord {
+  id: Uuid;
+  type: EntityType;
+}
+
 interface EntityBase {
   _id: Uuid;
   _version?: number;
